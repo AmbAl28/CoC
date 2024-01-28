e73533e54ea2e3a8559bc4a50e823f8d81f3c871
AmbAlex <a.leha@bk.ru>
2024-01-28 15:52:39 +0500

    Добавил машину с музыкой, опустил персонажа на землю

---
+++ Assets/Script/MusicController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    AudioSource audioSource;
+    Transform player; // Позиция игрока
+
+    public float maxDistance = 50f;
+
+    void Start()
+    {
+        // Проверяем, есть ли компонент AudioSource
+        audioSource = GetComponent<AudioSource>();
+
+        // Если компонент не найден, добавляем его
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            // Тут можно также установить другие параметры AudioSource, если необходимо
+        }
+
+        player = Camera.main.transform; // Предполагаем, что камера используется как слушатель
+        PlayMusic();
+    }
+
+    void Update()
+    {
+        // Обновляем расстояние между музыкой и игроком
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Применяем затухание на основе расстояния
+        audioSource.volume = 1 - Mathf.Clamp01(distanceToPlayer / maxDistance);
+    }
+
+    void PlayMusic()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+}
