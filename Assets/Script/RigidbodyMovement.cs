6e9ab37408216e3ff47587b57bb012af2f8a3cb3
AmbAlex <a.leha@bk.ru>
2024-02-26 04:47:30 +0300

    Много всего

--- Assets/Script/RigidbodyMovement.cs
+++ Assets/Script/RigidbodyMovement.cs
@@ -1,95 +1,111 @@
-using System;
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
 public class RigidbodyMovement : MonoBehaviour
 {
+    private float waterTime = 0f;
+    private Vector3 waterPosition = new Vector3(-24.29f, 26.169f, -3.46f);
     private new Rigidbody rigidbody;
     private Animator Anim;
     private bool isGrounded;
     private float jumpForce = 2f;
 
     [SerializeField] private PlayerUI ui;
-    private int _phone; //Экран телефона
+    private int _phone;
 
     [SerializeField] protected float movementSpeed = 2f;
     protected Vector3 movementVector;
 
+    private Coroutine waterTimeCoroutine; // Ссылка на корутину
+    private bool canControlCharacter = true; // Флаг, позволяющий управлять персонажем или блокировать управление
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         Anim = GetComponent<Animator>();
-        _phone = 0;//Установить экран телефона при старте
+        _phone = 0;
     }
 
     private void FixedUpdate()
     {
-        movementVector = (transform.right * Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") * transform.forward).normalized;
-        // Debug.Log(message: ("Vertical:", Input.GetAxis("Vertical"), " Horizontal:", Input.GetAxis("Horizontal")));
-        // Debug.Log(message: ("movementVector:", movementVector));
-        rigidbody.MovePosition(transform.position + movementVector * movementSpeed * Time.fixedDeltaTime);
-
-        Anim.SetBool("isWalk", Input.GetAxis("Vertical") > 0);// Код, который будет выполнен, если если персонаж двигается вперёд
-        Anim.SetBool("isBack", Input.GetAxis("Vertical") < 0);// Код, который будет выполнен, если если персонаж двигается назад
-        Anim.SetBool("isRight", Input.GetAxis("Horizontal") > 0);// Код, который будет выполнен, если персонаж двигается вправа
-        Anim.SetBool("isLeft", Input.GetAxis("Horizontal") < 0);// Код, который будет выполнен, если персонаж двигается влево
-
-        //Ускорение
-        if (isGrounded && Input.GetKey(KeyCode.LeftShift) && Input.GetAxis("Vertical") > 0)
+        if (canControlCharacter)
         {
-            movementSpeed = 4f;
-            Anim.SetBool("isShiftRun", Input.GetKey(KeyCode.LeftShift));
-        }
-        else
-        {
-            movementSpeed = 2f;
-            Anim.SetBool("isShiftRun", false);
-        }
+            movementVector = (transform.right * Input.GetAxis("Horizontal") + Input.GetAxis("Vertical") * transform.forward).normalized;
+            // Debug.Log(message: ("Vertical:", Input.GetAxis("Vertical"), " Horizontal:", Input.GetAxis("Horizontal")));
+            // Debug.Log(message: ("movementVector:", movementVector));
+            rigidbody.MovePosition(transform.position + movementVector * movementSpeed * Time.fixedDeltaTime);
 
-        // Удар по зажатии левой кнопки мыши
-        if (Input.GetMouseButton(0) && isGrounded && !(Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0 || Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0))
-        {
-            Anim.SetBool("isCombat", true);
-            //     Hit();
-        }
-        else
-        {
-            Anim.SetBool("isCombat", false);
-        }
+            Anim.SetBool("isWalk", Input.GetAxis("Vertical") > 0);// Код, который будет выполнен, если персонаж двигается вперёд
+            Anim.SetBool("isBack", Input.GetAxis("Vertical") < 0);// Код, который будет выполнен, если персонаж двигается назад
+            Anim.SetBool("isRight", Input.GetAxis("Horizontal") > 0);// Код, который будет выполнен, если персонаж двигается вправа
+            Anim.SetBool("isLeft", Input.GetAxis("Horizontal") < 0);// Код, который будет выполнен, если персонаж двигается влево
 
-        //Anim.SetBool(name: "isWalk", value: movementVector.magnitude > 0.1f);
-
-        //Перекат 
-        // if (isGrounded && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)))
-        // {
-        //     Anim.SetBool("isRoll", Input.GetKey(KeyCode.LeftControl));
-        // }
-        // else Anim.SetBool("isRoll", false);
-
-        //Падение, подрыгнуть
-        if (isGrounded && Input.GetKey(KeyCode.Space))
-        {
-            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            Anim.SetBool("isJump", true);
-        }
-
-        //Достать инвентарь
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            // Debug.Log(message: Nphone);
-            // Debug.Log(message: _phone);
-            _phone = _phone + 1; //Переключаем экраны на телефоне
-            ui.SetPhone(_phone); //Местод вызывающий включение или выключение экрана
-
-            if (_phone >= 4)
+            // Проверяем, достиг ли счетчик waterTime значения 10 (время в воде)
+            if (waterTime >= 10f)
             {
-                _phone = 0;
-                ui.SetPhone(_phone);
-                Debug.Log(message: "Телефон убрал");
+                // Если достиг, меняем позицию персонажа, запускает запрет на движение, до окончание анимации
+                StartCoroutine(WaitAnimationFinish());
+            }
+
+            //Ускорение
+            if (isGrounded && Input.GetKey(KeyCode.LeftShift) && Input.GetAxis("Vertical") > 0)
+            {
+                movementSpeed = 4f;
+                Anim.SetBool("isShiftRun", Input.GetKey(KeyCode.LeftShift));
+            }
+            else
+            {
+                movementSpeed = 2f;
+                Anim.SetBool("isShiftRun", false);
+            }
+
+            // Удар по зажатии левой кнопки мыши
+            if (Input.GetMouseButton(0) && isGrounded && !(Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0 || Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0))
+            {
+                Anim.SetBool("isCombat", true);
+                //     Hit();
+            }
+            else
+            {
+                Anim.SetBool("isCombat", false);
+            }
+
+            //Падение, подрыгнуть
+            if (isGrounded && Input.GetKey(KeyCode.Space))
+            {
+                rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                Anim.SetBool("isJump", true);
+            }
+
+            // //Проверка что персонаж на тиррейне
+            // RaycastHit hit;
+            // if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            // {
+            //     if (hit.collider.CompareTag("Ground"))
+            //     {
+            //         // Персонаж находится на террейне
+            //         isGrounded = true;
+            //         Debug.Log("ТЕГ На земле");
+            //     }
+            // }
+
+            //Достать инвентарь
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                // Debug.Log(message: Nphone);
+                // Debug.Log(message: _phone);
+                _phone = _phone + 1; //Переключаем экраны на телефоне
+                ui.SetPhone(_phone); //Местод вызывающий включение или выключение экрана
+
+                if (_phone >= 4)
+                {
+                    _phone = 0;
+                    ui.SetPhone(_phone);
+                    Debug.Log(message: "Телефон убрал");
+                }
             }
         }
-
     }
 
     //Проверка находится ли персонаж на земле
@@ -97,11 +113,64 @@ public class RigidbodyMovement : MonoBehaviour
     {
         // Считаем персонажа на земле после любого столкновения
         isGrounded = true;
-        Anim.SetBool(name: "isJump", value: false);
+        Anim.SetBool("isJump", false);
+        Debug.Log("На земле");
     }
     private void OnCollisionExit(Collision collision)
     {
         // При отсутствии столкновения считаем, что персонаж не на земле
         isGrounded = false;
+        Debug.Log("Не на земле");
+
+    }
+
+    //Плаванье и корутина (счётчик) для увеличения waterTime каждую секунду в воде
+    IEnumerator IncreaseWaterTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            waterTime += 1f;
+            Debug.Log("Water Time: " + waterTime);
+        }
+
+    }
+    private IEnumerator WaitAnimationFinish()
+    {
+        Anim.SetBool("isSit", true);
+        transform.position = waterPosition;
+        canControlCharacter = false; // Запрещаем управление персонажем и камерой при начале анимации
+        Debug.Log("Запрещено двигаться");
+        yield return new WaitForSeconds(13f); // Сколько ждём (заменить на фактическую длину анимации)
+        Debug.Log("Можно двигаться");
+        Anim.SetBool("isSit", false);
+        canControlCharacter = true; // Разрешаем управление персонажем и камерой после окончания анимации
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            // Запускаем корутину, если она еще не была запущена
+            if (waterTimeCoroutine == null)
+                waterTimeCoroutine = StartCoroutine(IncreaseWaterTime());
+            Anim.SetBool("isSwim", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            // Останавливаем корутину, если она была запущена
+            if (waterTimeCoroutine != null)
+            {
+                StopCoroutine(waterTimeCoroutine);
+                waterTimeCoroutine = null;
+            }
+
+            Anim.SetBool("isSwim", false);
+            waterTime = 0f; // Сбрасываем счетчик времени в воде
+        }
     }
 }
